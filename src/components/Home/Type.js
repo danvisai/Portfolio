6a986211512b050f353ee4a09e2233e872cebee7
danvisai <70681628+danvisai@users.noreply.github.com>
2022-11-14 20:32:03 +0530

    typescript

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,8 +7,8 @@ function Type() {
       options={{
         strings: [
           "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
+          "PCCET certified| Cybersecurity Enthusiast",
+          "Blockchain Developer",
           "Open Source Contributor",
         ],
         autoStart: true,
