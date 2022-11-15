0c9b496dc1ec53f93ee70f7f6327b99dd6efbf7b
danvisai <70681628+danvisai@users.noreply.github.com>
2022-11-15 19:56:43 +0530

    projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
+import ds from "../../Assets/Projects/ds.png";
+import ig from "../../Assets/Projects/ig.png";
 import marketplace from "../../Assets/Projects/marketplace.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import pb from "../../Assets/Projects/pb.png";
+import aqp from "../../Assets/Projects/aqp.png";
 
 function Projects() {
   return (
@@ -34,7 +33,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={aqp}
               isBlog={false}
               title="Automatic-Question-Paper-Generator"
               description="Automatic Question Paper Generator made with jsp, java and Css.Lets users creates quizzes and attend them."
@@ -45,7 +44,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={ig}
               isBlog={false}
               title="ImageSteganography"
               description="ImageSteganography prject made wiht python and flask. Takes an image as an input and uses it to hide a message using cryptography techniques by changing pixels of the image"
@@ -56,7 +55,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={ds}
               isBlog={false}
               title="CrimeAnalysis"
               description="A DataScience project made using python to analyze crime rate in a country. I have used visualization techniques and filtering techniques in the project"
@@ -67,7 +66,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={pb}
               isBlog={false}
               title="PUBLICATION"
               description="Relevance of Computer Forensics in Security"
