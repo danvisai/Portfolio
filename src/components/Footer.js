4994f651f772b17177526703711b60abbe02b1ae
danvisai <70681628+danvisai@users.noreply.github.com>
2022-11-21 16:09:38 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,10 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by S DANVI SAI</h3>
-        </Col>
-        <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} DanviSai</h3>
+          <h3></h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
