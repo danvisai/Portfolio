97224d74ab6b41e161a0f9e10bbe16be6c9a30c4
danvisai <70681628+danvisai@users.noreply.github.com>
2022-11-28 14:14:26 +0530

    home2

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,8 +19,7 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I fell in love with programming and I started exploring, 🔍
               <br />
               <br />I am fluent in classics like
               <i>
