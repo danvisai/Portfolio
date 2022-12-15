5900a7cbd43b4ad3e42016edef227e7d66fd8c25
luffy <70351871+git-prudhvi@users.noreply.github.com>
2022-12-15 15:57:48 +0530

    update resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/danvisai/Portfolio/main/src/Assets/S%20Danvi%20Sai%20Sapthasw%20Reddy.pdf";
+  "https://raw.githubusercontent.com/danvisai/Portfolio/main/src/Assets/S_Danvi_Sai_Sapthasw_Reddy.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
