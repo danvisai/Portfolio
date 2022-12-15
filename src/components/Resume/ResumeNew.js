2e19d4a06710f50608ab64462875c6aee41a62a2
luffy <70351871+git-prudhvi@users.noreply.github.com>
2022-12-15 15:59:17 +0530

    Update Resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/S_Danvi_Sai_Sapthasw_Reddy.pdf";
+import pdf from "../../Assets/../Assets/S_Danvi_Sapthasw_Reddy.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/danvisai/Portfolio/main/src/Assets/S_Danvi_Sai_Sapthasw_Reddy.pdf";
+  "https://raw.githubusercontent.com/danvisai/Portfolio/main/src/Assets/S_Danvi_Sapthasw_Reddy.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
