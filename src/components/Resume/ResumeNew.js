1d1af44314f44786c1791d7f6c4d7472456ea0df
luffy <70351871+git-prudhvi@users.noreply.github.com>
2022-12-15 15:58:19 +0530

    Update Resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/S Danvi Sai Sapthasw Reddy.pdf";
+import pdf from "../../Assets/../Assets/S_Danvi_Sai_Sapthasw_Reddy.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
