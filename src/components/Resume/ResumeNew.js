7952a4a735da6c563d6515bd01047bbd87b4b05e
danvisai <70681628+danvisai@users.noreply.github.com>
2023-11-05 00:56:27 -0400

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/danvisai/Portfolio/main/src/Assets/S_Danvi_Sapthasw_Reddy.pdf";
+  "https://raw.githubusercontent.com/danvisai/Portfolio/main/src/Assets/resume_danvi.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
