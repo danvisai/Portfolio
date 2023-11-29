4461debdda45445a487a1e28a6461b073670c213
danvisai <70681628+danvisai@users.noreply.github.com>
2023-11-29 00:19:24 -0500

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Danvi Sai </span>
+            Hi Everyone, I am <span className="purple">Danvi</span>
              <span className="purple"> .</span>
             <br />I am a student at Purdue University pursuing masters in computer graphics technology.
             <br />
