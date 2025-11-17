fb48e06616be043cee54274fdd4d72ed0eff4609
danvisai <70681628+danvisai@users.noreply.github.com>
2025-11-16 22:00:09 -0500

    Update resume link to new PDF file

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/danvisai/Portfolio/main/src/Assets/Danvi Sai Sapthasw Reddy.pdf";
+  "https://raw.githubusercontent.com/danvisai/Portfolio/main/src/Assets/Danvi_Simhadri_Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
