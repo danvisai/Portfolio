b1239ae38b8d0e22b50aef18fe8cf12402291354
danvialt <70681628+danvisai@users.noreply.github.com>
2026-03-06 17:46:04 -0500

    Add GA tag and ignore local/archive artifacts

--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,6 +1,36 @@
 import React from "react";
-function Pre(props) {
-  return <div id={props.load ? "preloader" : "preloader-none"}></div>;
+import blockIcon from "../Assets/icon_block.svg";
+import mushroomIcon from "../Assets/icon_mushroom.svg";
+import marioRunning from "../Assets/mario_running.png";
+import terrainTile from "../Assets/terrain_block_tile.png";
+
+function Pre({ load }) {
+  return (
+    <div id={load ? "preloader" : "preloader-none"}>
+      <div className="mario-pre-center">
+        <div className="mario-pre-icons">
+          <img src={blockIcon} alt="" className="mario-pre-block" />
+          <img src={mushroomIcon} alt="" className="mario-pre-mushroom" />
+        </div>
+        <p className="mario-pre-label">LOADING</p>
+        <div className="mario-pre-dots">
+          <span />
+          <span />
+          <span />
+        </div>
+        <div className="mario-pre-bar-wrap">
+          <div className="mario-pre-bar" />
+        </div>
+      </div>
+      <div className="mario-pre-ground-wrap">
+        <img src={marioRunning} alt="Mario" className="mario-pre-mario" />
+        <div
+          className="mario-pre-ground"
+          style={{ backgroundImage: `url(${terrainTile})` }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Pre;
